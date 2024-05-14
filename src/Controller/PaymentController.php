e50d6b977d993cdbdafd972807e4fa61bed5b90a
AdemSansa <adem.sansa7@gmail.com>
2024-05-15 00:17:45 +0100

    payement fixed

--- src/Controller/PaymentController.php
+++ src/Controller/PaymentController.php
@@ -4,11 +4,14 @@ namespace App\Controller;
 
 use Stripe\Stripe;
 use Stripe\Checkout\Session;
+use App\Repository\LivreRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
+#[IsGranted('ROLE_USER')]
 class PaymentController extends AbstractController
 {
     #[Route('/payment', name: 'app_payment')]
@@ -20,28 +23,39 @@ class PaymentController extends AbstractController
     }
 
     #[Route('/checkout', name: 'checkout')]
-    public function checkout(): Response
+    public function checkout(SessionInterface $ses,LivreRepository $Lp): Response
     {
+        
         Stripe::setApiKey("sk_test_VePHdqKTYQjKNInc7u56JBrQ");
-
+        $panier = $ses->get('panier', []);
+        $t=[];
+        foreach($panier as $item => $qte)
+        {
+            $livre  =  $Lp->find($item);
+            $prix = $livre->getPrix();
+            $t[] = [ // Append a new element to $t
+                'price_data' => [
+                    'currency' => 'usd',
+                    'product_data' => [
+                        'name' => $livre->getTitre(),
+                    ],
+                    'unit_amount' => $prix,
+                ],
+                'quantity' => $qte,
+            ];
+            
+        }
         $session = Session::create([
             'payment_method_types' => ['card'],
             'line_items'           => [
-                [
-                    'price_data' => [
-                        'currency'     => 'usd',
-                        'product_data' => [
-                            'name' => 'T-shirt',
-                        ],
-                        'unit_amount'  => 2000,
-                    ],
-                    'quantity'   => 1,
-                ]
+                $t
             ],
             'mode'                 => 'payment',
             'success_url'          => $this->generateUrl('success_url', [], UrlGeneratorInterface::ABSOLUTE_URL),
             'cancel_url'           => $this->generateUrl('cancel_url', [], UrlGeneratorInterface::ABSOLUTE_URL),
         ]);
+        $ses->remove('panier');
+        
 
         return $this->redirect($session->url, 303);
     }
