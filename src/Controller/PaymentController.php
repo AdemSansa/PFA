d06e684dc490c729cc17cc4ef764cb559921e098
youssefrayen123 <yousseffehmi96@gmail.com>
2024-05-15 06:04:18 +0100

    brande

--- src/Controller/PaymentController.php
+++ src/Controller/PaymentController.php
@@ -8,9 +8,11 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+#[IsGranted('ROLE_USER')]
 class PaymentController extends AbstractController
 {
+    
     #[Route('/payment', name: 'app_payment')]
     public function index(): Response
     {
@@ -50,13 +52,13 @@ class PaymentController extends AbstractController
     #[Route('/success-url', name: 'success_url')]
     public function successUrl(): Response
     {
-        return $this->render('payment/succes.html.twig', []);
+        return $this->render('payment/succes.html.twig');
     }
 
 
     #[Route('/cancel-url', name: 'cancel_url')]
     public function cancelUrl(): Response
     {
-        return $this->render('payment/cancel.html.twig', []);
+        return $this->render('payment/cancel.html.twig');
     }
 }
