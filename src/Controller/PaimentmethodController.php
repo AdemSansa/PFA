2fe6641c3eabc3c6dead402769810196336bf290
youssefrayen123 <yousseffehmi96@gmail.com>
2024-05-21 20:02:21 +0100

    pay

---
+++ src/Controller/PaimentmethodController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class PaimentmethodController extends AbstractController
+{
+    #[Route('/paimentmethod', name: 'app_paimentmethod')]
+    public function index(): Response
+    {
+        return $this->render('paimentmethod/index.html.twig', [
+            'controller_name' => 'PaimentmethodController',
+        ]);
+    }
+
+    #[Route('/modepaiment', name: 'app_modepaiment')]
+    public function modepaiment(Request $request): Response
+    {
+        $selectedPayment = $request->request->get('modePaiement');
+        if($selectedPayment==="Paiement à la livraison"){
+            return $this->redirectToRoute('checkout');
+        }
+        else{
+            return $this->render('payment/succe.html.twig'
+        );
+        }
+        
+    }
+
+
+
+}
